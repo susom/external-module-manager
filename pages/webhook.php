b3c43cc6e173f13751680245370448428a31a0b8
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-04-07 12:42:23 -0700

    test secret

--- pages/webhook.php
+++ pages/webhook.php
@@ -5,6 +5,13 @@ namespace Stanford\ExternalModuleManager;
 
 
 try {
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+    $rawPost = file_get_contents('php://input');
+    $xx = 'RYL8udHvqGcbkjXk8sPXi3zGhCQGyHqGa7utjPgG';
+    $yourHash = base64_encode(hash_hmac('sha1', $_POST['payload'], $xx));
+    if (!hash_equals($hash, hash_hmac($algo, $rawPost, $xx))) {
+        throw new \Exception('Hook secret does not match.');
+    }
     # test commit
     if (isset($_POST) && !empty($_POST)) {
         $payload = json_decode($_POST['payload'], true);
