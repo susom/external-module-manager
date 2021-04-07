df024440b8b08298f07aa0a12e22b5c5b96f365c
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-04-07 12:56:31 -0700

    exit when EM is found and updated.

--- ExternalModuleManager.php
+++ ExternalModuleManager.php
@@ -97,7 +97,7 @@ class ExternalModuleManager extends \ExternalModules\AbstractExternalModule
                     echo '<pre>';
                     print_r($response);
                     echo '</pre>';
-                    $this->exitAfterHook();
+                    die();
                 } else {
                     throw new \Exception("cant update last commit for EM : " . $repository[$this->getFirstEventId()]['module_name']);
                 }
