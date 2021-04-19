ce881d4d9a1a0a5395a89e2de6f20e99c7c23ef7
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-04-19 09:09:00 -0700

    log EMs utilization using REDCap Entity table.

---
+++ pages/test_entities.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Stanford\ExternalModuleManager;
+
+use REDCapEntity\EntityList;
+
+/** @var ExternalModuleManager $module */
+
+#$module->createExternalModuleUtilizationLogs();
+
+$list = new EntityList('external_modules_utilization', $module);
+$list->setOperations(['create', 'update', 'delete'])
+    ->render('project'); // Context: project.
