ba764a457514978db015a19a638d07462229f3d6
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-04-26 13:09:13 -0700

    pull next 30 days instead of end of current month.

--- pages/test_entities.php
+++ pages/test_entities.php
@@ -16,6 +16,18 @@ use REDCapEntity\EntityList;
 
 #$module->createProjectsExternalModuleUsageLogs();
 
-$list = new EntityList('project_external_modules_usage', $module);
-$list->setOperations(['create', 'update', 'delete'])
-    ->render('project'); // Context: project.
+//$list = new EntityList('project_external_modules_usage', $module);
+//$list->setOperations(['create', 'update', 'delete'])
+//    ->render('project'); // Context: project.
+
+$data = array(
+    'module_prefix' => '',
+    'project_id' => '',
+    'project_title' => '',
+    'status' => 123,
+    'record_count' => 'asd',
+    'is_em_enabled' => 123,
+    'number_of_settings_rows' => 'asd',
+);
+
+$result = $module->getEntityFactory()->create('external_modules_utilization', $data);
